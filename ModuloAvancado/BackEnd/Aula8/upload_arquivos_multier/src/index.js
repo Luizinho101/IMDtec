cf32893fcb5f7e27d9eafdc274b65abe54727b40
Luizinho101 <luiz.antonio.da.silva777@gmail.com>
2026-04-04 22:07:51 -0300

    feat: Cria o Upload de arquivos na api node

---
+++ ModuloAvancado/BackEnd/Aula8/upload_arquivos_multier/src/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const rotaUsuario = require('./rotas/usuario.rota')
+const rotaPost = require('./rotas/post.rota')
+
+const app = express()
+app.use(express.json())
+
+const PORT = 8080
+
+  app.use('/static', express.static('public'))
+app.use('/usuarios', rotaUsuario)
+app.use('/posts', rotaPost)
+
+app.get('/', (req, res) => {
+    res.json({msg: "Hello from Express!"})
+})
+
+const server = app.listen(PORT)
+
+server.on('listening', () => {
+    console.log(`Servidor pronto na porta ${PORT}`)
+})
+
+server.on('error', (err) => {
+    if (err.code === 'EADDRINUSE') {
+        console.error(`Erro: a porta ${PORT} já está em uso. Use outra porta ou encerre o processo que a está usando.`)
+    } else {
+        console.error('Erro ao iniciar o servidor:', err.message)
+    }
+    process.exit(1)
+})
