7750f91ac097d2dab3bf6954164a3fc252959055
Luizinho101 <luiz.antonio.da.silva777@gmail.com>
2026-05-09 20:28:11 -0300

    feat: adicionar alterações da aula 1

---
+++ ModuloAvancado/Desenvolvimento_para_Dispositivos_Moveis/Aula1/aula01/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const element =  <h1> Olá mundo !</h1>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  element,
+);
