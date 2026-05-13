892e6118e7852c61e97e1ed9e237f5bf2a273b11
Luizinho101 <luiz.antonio.da.silva777@gmail.com>
2026-05-12 22:56:26 -0300

    feat: criar component de classe

---
+++ ModuloAvancado/Desenvolvimento_para_Dispositivos_Moveis/Aula2/relogio-classcomponent/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+class ClockClass extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { date: new Date() }
+  }
+
+  componentDidMount() {
+    this.timerId = setInterval(() => {
+      this.setState({ date: new Date() });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId)
+  }
+
+
+  render() {
+    return (<div><h1>Hora: {this.state.date.toLocaleTimeString()} </h1></div>);
+  };
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <ClockClass />
+);
+
+
