4b7e3e8b0650f3d3bca1953dc528fb2e6ad19c36
Luizinho101 <luiz.antonio.da.silva777@gmail.com>
2026-06-10 22:32:37 -0300

    feat: estrutura inicial da aula de imagens e resoluções (@2x e @3x)

---
+++ ModuloAvancado/Desenvolvimento_para_Dispositivos_Moveis/Aula10/aula-imagens-resolucao/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, View, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image source={require('./assets/college/college.png')} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
