817f6d6f2c528379714ef7d96155ec455ebda22d
Luizinho101 <luiz.antonio.da.silva777@gmail.com>
2026-06-11 12:25:27 -0300

    feat: adiciona projeto de práticas com imagens online no Expo

---
+++ ModuloAvancado/Desenvolvimento_para_Dispositivos_Moveis/Aula10/pratica-img-expo-online/praticas.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View , Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image source={require('./assets/college/college.png')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
